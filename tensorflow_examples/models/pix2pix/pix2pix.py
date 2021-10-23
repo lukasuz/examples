41abb55fae516d0b3842f90cf502dbf4d5a71442
Lukas Uzolas <lukas@uzolas.com>
2021-10-23 15:48:33 +0200

    v3

--- tensorflow_examples/models/pix2pix/pix2pix.py
+++ tensorflow_examples/models/pix2pix/pix2pix.py
@@ -399,6 +399,93 @@ def unet_generator_dual(gen_channels, disc_channels, norm_type='batchnorm'):
   return tf.keras.Model(inputs=inputs, outputs=[out_gen, out_disc])
 
 
+def unet_generator_v3(gen_channels, disc_channels, norm_type='batchnorm'):
+  """Modified u-net generator model (https://arxiv.org/abs/1611.07004).
+
+  Args:
+    output_channels: Output channels
+    norm_type: Type of normalization. Either 'batchnorm' or 'instancenorm'.
+
+  Returns:
+    Generator model
+  """
+
+  down_stack = [
+      downsample(64, 4, norm_type, apply_norm=False),  # (bs, 128, 128, 64)
+      downsample(128, 4, norm_type),  # (bs, 64, 64, 128)
+      downsample(256, 4, norm_type),  # (bs, 32, 32, 256)
+      downsample(512, 4, norm_type),  # (bs, 16, 16, 512)
+      downsample(512, 4, norm_type),  # (bs, 8, 8, 512)
+      downsample(512, 4, norm_type),  # (bs, 4, 4, 512)
+      downsample(512, 4, norm_type),  # (bs, 2, 2, 512)
+      downsample(512, 4, norm_type),  # (bs, 1, 1, 512)
+  ]
+
+  up_stack_gen = [
+      upsample(512, 4, norm_type, apply_dropout=True),  # (bs, 2, 2, 1024)
+      upsample(512, 4, norm_type, apply_dropout=True),  # (bs, 4, 4, 1024)
+      upsample(512, 4, norm_type, apply_dropout=True),  # (bs, 8, 8, 1024)
+      upsample(512, 4, norm_type),  # (bs, 16, 16, 1024)
+      upsample(256, 4, norm_type),  # (bs, 32, 32, 512)
+      upsample(128, 4, norm_type),  # (bs, 64, 64, 256)
+      upsample(64, 4, norm_type),  # (bs, 128, 128, 128)
+  ]
+
+  initializer = tf.random_normal_initializer(0., 0.02)
+  last_gen = tf.keras.layers.Conv2DTranspose(
+      gen_channels, 4, strides=2,
+      padding='same', kernel_initializer=initializer,
+      activation='tanh')  # (bs, 256, 256, 3)
+
+  concat = tf.keras.layers.Concatenate()
+
+  inputs = tf.keras.layers.Input(shape=[None, None, 3])
+  x = inputs
+
+  # Downsampling through the model
+  skips = []
+  for down in down_stack:
+    x = down(x)
+    skips.append(x)
+  
+  x_gen = x
+  skips = reversed(skips[:-1])
+
+  # Upsampling
+  for i in range(len(up_stack_gen)):
+    up_gen = up_stack_gen[i]
+    skip = skips[i]
+    x_gen = up_gen(x_gen)
+    x_gen = concat([x_gen, skip])
+
+  out_gen = last_gen(x_gen)
+
+  # Discriminator
+  down3 = down_stack[2] # equivalent to down3
+  zero_pad1 = tf.keras.layers.ZeroPadding2D()(down3)  # (bs, 34, 34, 256)
+  conv = tf.keras.layers.Conv2D(
+      512, 4, strides=1, kernel_initializer=initializer,
+      use_bias=False)(zero_pad1)  # (bs, 31, 31, 512)
+
+  if norm_type.lower() == 'batchnorm':
+    norm1 = tf.keras.layers.BatchNormalization()(conv)
+  elif norm_type.lower() == 'instancenorm':
+    norm1 = InstanceNormalization()(conv)
+
+  leaky_relu = tf.keras.layers.LeakyReLU()(norm1)
+
+  zero_pad2 = tf.keras.layers.ZeroPadding2D()(leaky_relu)  # (bs, 33, 33, 512)
+
+  out_disc = tf.keras.layers.Conv2D(
+      1, 4, strides=1,
+      kernel_initializer=initializer)(zero_pad2)  # (bs, 30, 30, 1)
+
+
+
+
+  return tf.keras.Model(inputs=inputs, outputs=[out_gen, out_disc])
+
+
 def discriminator(norm_type='batchnorm', target=True):
   """PatchGan discriminator model (https://arxiv.org/abs/1611.07004).
 
